300b69f8a119b6ed764170f96e6a0102fa395c08
ojustine <ojustine@student.21-school.ru>
2021-02-28 16:42:14 +0300

    vm add useful step dump

--- src/vm/vm_run.c
+++ src/vm/vm_run.c
@@ -1,30 +1,44 @@
+#include "io_.h"
+#include "str.h"
+#include "conv.h"
 #include "ft_printf.h"
 #include "util.h"
 #include "vm.h"
 
+void	vm_dump(void)
+{
+	char *user_input;
+
+	print_arena();
+	if (g_vm.config & VM_STEP_DUMP)
+	{
+		if (get_next_line(0, &user_input) == -1)
+			ft_exit(EXIT_FAILURE, "Reading from STDIN is failed");
+		log_info(__func__, "User input: %s", user_input);
+		if (ft_isuint(user_input))
+			g_vm.dump_cycle += (int)ft_atol(user_input);
+		else
+			g_vm.dump_cycle += g_vm.dump_cycle;
+		return ;
+	}
+	ft_exit(EXIT_SUCCESS, "Stop Corewar Virtual Machine after dump");
+}
+
 void	vm_run(void)
 {
-	int	dump_exit;
-
 	log_info(__func__, "Run Corewar Virtual Machine");
 	vm_cursor_set_initial();
 	print_intro();
-	dump_exit = 0;
 	while (g_vm.cursors->size)
 	{
-		if (g_vm.config & VM_DUMP && g_vm.dump_cycle == g_vm.cycles)//!(g_vm.cycles % g_vm.dump_cycle))todo step
-		{
-			dump_exit = 1;
-			print_arena();
-			break ;
-		}
+		if (g_vm.config & VM_DUMP && g_vm.dump_cycle == g_vm.cycles)
+			vm_dump();
 		vm_exec();
 		if (g_vm.cycles_to_die == g_vm.cycles_after_check
 			|| g_vm.cycles_to_die <= 0)
 			vm_check();
 	}
-	if (!dump_exit)
-		print_winner();
+	print_winner();
 }
 
 int		main(int ac, char **av)
